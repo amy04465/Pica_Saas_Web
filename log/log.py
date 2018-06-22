72bbc87a53dba24cbc5521c6e6946ec403269795
Amy <979961272@qq.com>
2018-06-22 23:27:56 +0800

    update

--- log/log.py
+++ log/log.py
@@ -6,33 +6,63 @@ __author__ = amy liu
 
 # 日志类
 
-import logging
-import logging.handlers
-
-class Logger():
-    LOG_FILE = 'test.log'
-
-    # 实例化 handler
-    handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes = 1024*1024, backupCount = 5)
-    fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-
-    # 实例化 formatter
-    formatter = logging.Formatter(fmt)
-    # 为 handler 添加 formatter
-    handler.setFormatter(formatter)
-
-    # 获取名 为 test 的logger
-    logger = logging.getLogger('test')
-    # 为 logger 添加 handler
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-
-    def loginfo(self, message):
-        self.logger.info(message)
-
-    def logdebug(self, message):
-        self.logger.debug(message)
+# coding:utf-8
+import logging,time,os
+# 这个是日志保存本地的路径
+log_path = "./report"
+test_dir = './test_case'
 
 
+class Log:
+    def __init__(self):
+        # 文件的命名
+        self.logname = os.path.join(log_path, '%s.log'%time.strftime('%Y_%m_%d'))
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)
+        # 日志输出格式
+        self.formatter = logging.Formatter('[%(asctime)s] - %(filename)s[line:%(lineno)d] - fuc:%(funcName)s- %(levelname)s: %(message)s')
 
+    def __console(self, level, message):
+        # 创建一个FileHandler，用于写到本地
+        fh = logging.FileHandler(self.logname, 'a')  # 追加模式
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(self.formatter)
+        self.logger.addHandler(fh)
 
+        # 创建一个StreamHandler,用于输出到控制台
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(self.formatter)
+        self.logger.addHandler(ch)
+
+        if level == 'info':
+            self.logger.info(message)
+        elif level == 'debug':
+            self.logger.debug(message)
+        elif level == 'warning':
+            self.logger.warning(message)
+        elif level == 'error':
+            self.logger.error(message)
+        # 这两行代码是为了避免日志输出重复问题
+        self.logger.removeHandler(ch)
+        self.logger.removeHandler(fh)
+        # 关闭打开的文件
+        fh.close()
+
+    def debug(self, message):
+        self.__console('debug', message)
+
+    def info(self, message):
+        self.__console('info', message)
+
+    def warning(self, message):
+        self.__console('warning', message)
+
+    def error(self, message):
+        self.__console('error', message)
+
+if __name__ == "__main__":
+   log = Log()
+   log.info("---测试开始----")
+   log.info("输入密码")
+   log.warning("----测试结束----")
