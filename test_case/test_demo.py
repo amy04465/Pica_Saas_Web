9423479cbd4d4fc665f60abb658e10df0bfe2009
amy <979961272@qq.com>
2018-06-28 13:27:29 +0800

    关于classmethod、staticmethod和普通方法的区别

--- test_case/test_demo.py
+++ test_case/test_demo.py
@@ -3,23 +3,59 @@ __data__ = 2017/11/24
 __author__ = amy liu
 '''
 
-import time
-import unittest
-from selenium import webdriver
 
-from page_obj.function import search
+# 对于普通方法
+# 当实例调用时，默认将当前实例传进去
+# 类调用时，只能以 类名.method(类实例) 形式调用
+class DemoClass1:
+    def classPrint(self):
+        print("class method")
+
+    def objPrint(self):
+        print("obj method")
+
+# 必须实例化
+obj = DemoClass1()
+
+# 通过 实例 来调用函数
+DemoClass1.classPrint(obj)
+DemoClass1.objPrint(obj)
+
+# 通过 类 来调用函数
+obj.classPrint()
+obj.objPrint()
+
+# 对于classmethod方法
+# 当实例调用classmethod方法时，默认会把当前实例所对应的类传进去
+# 当类调用classmethod方法时，默认把此类传进去
+class DemoClass2:
+    @classmethod
+    def classPrint(self):
+        print("class method")
+
+    def objPrint(self):
+        print("obj method")
 
 
-class KeywordsSearch(unittest.TestCase):
-    def setUp(self):
-        print('pass')
+# 当实例调用classmethod方法时，默认会把当前实例所对应的类传进去
+# 不需要实例化，不需要加self
+DemoClass2.classPrint()
+# 当类调用classmethod方法时，默认把此类传进去
+obj = DemoClass2()
+obj.classPrint()
 
-    def test_KeywordsSearch(self):
-        search('haowan')
 
-    def tearDown(self):
-        pass
+# 对于staticmethod方法
+# 实例和类调用，没有默认的参数传进函数
+class DemoClass3:
+    @staticmethod
+    # 不需要传入参数
+    def classPrint():
+        print("class method")
 
-if __name__ == '__main__':
-    unittest.main()
+    def objPrint(self):
+        print("obj method")
+
+
+DemoClass3.classPrint()
 
