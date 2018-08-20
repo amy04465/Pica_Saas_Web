a2b6bcaf85cfcbf5bfcde2eb7b2fc092add32fe7
amy <979961272@qq.com>
2018-08-20 12:34:33 +0800

    增加 重点项目 测试用例

--- page_obj/HomePage.py
+++ page_obj/HomePage.py
@@ -15,8 +15,7 @@ class HomePage(BasePage):
     # 菜单栏
     studyDropMenu_loc = (By.XPATH, './/span[text()="一站培训"]')
     studyMenu_loc = (By.XPATH, './/a[text()="微专业"]')
-    #studyMenu_loc = (By.XPATH, '//div[1]/div[1]/div/div/div[2]/ul/li[2]/ul/li[3]/a')
-
+    # studyMenu_loc = (By.XPATH, '//div[1]/div[1]/div/div/div[2]/ul/li[2]/ul/li[3]/a')
     patientManage_loc = (By.LINK_TEXT, u'健康管理')
     patientEdu_loc = (By.XPATH, './/a[text()="健康漫画"]')
     medical_loc = (By.LINK_TEXT, u'诊疗助手')
@@ -25,7 +24,11 @@ class HomePage(BasePage):
     loginButton_loc = (By.XPATH, '//*[@id="login"]/li[1]/span')
     loginDialog_loc = (By.XPATH,'//div[@ class="modal-content"]')
     closeIcon_loc = (By.XPATH, '//*[@id = "loginClose"]')
-
+    # 轮播图
+    carouselImg_loc = (By.XPATH, '//div[@class="carousel-inner"]/div/img')
+    # 重点项目/html/body/div[3]/div/div/div/div/div[3]/img
+    mainProjectLeft_loc = (By.XPATH, '//*[@id="myCarousel-major"]/div/div/div[2]/img')
+    mainProjectRight_loc = (By.XPATH, '//*[@id="myCarousel-major"]/div/div/div[3]/img')
 
 
     # 点击[患者管理]
@@ -64,6 +67,26 @@ class HomePage(BasePage):
         print(u'关闭登录窗口')
         self.driver.find_element(*HomePage.closeIcon_loc).click()
         time.sleep(2)
+
+    # 轮播图
+    # def clickCarouselImg(self):
+    #     print(u'循环点击轮播图')
+    #     carouselImg = self.driver.find_elements(*HomePage.carouselImg_loc).click()
+    #     print(carouselImg.text)
+    #     time.sleep(8)
+    #     return carouselImg
+
+    # 重点项目
+    def goMainProjectLeft(self):
+        print(u'点击重点项目-- 高血压办公室')
+        self.driver.find_element(*HomePage.mainProjectLeft_loc).click()
+        time.sleep(3)
+
+    def goMainProjectRight(self):
+        print(u'点击重点项目-- 高危筛查')
+        self.driver.find_element(*HomePage.mainProjectRight_loc).click()
+        time.sleep(3)
+
 
 
     # 断言：显示登录弹窗
--- test_case/test_HomePage.py
+++ test_case/test_HomePage.py
@@ -6,7 +6,9 @@ __author__ = amy liu
 # coding = utf-8
 import unittest
 from congif.Mytest import Mytest
+from congif.encapsulation import switch_window
 from page_obj.HomePage import HomePage
+import time
 
 
 class HomePageTest(Mytest):
@@ -45,6 +47,34 @@ class HomePageTest(Mytest):
         # 点击[关于云鹊医]
         self.homePage.goAboutUs()
         self.assertIn('pica_about_us.html', self.homePage.get_currentUrl(), u'跳转链接错误')
+
+    # def test_clickCarouselImg(self):
+    #     # 循环点击轮播图
+    #     allcarouselImg = self.homePage.clickCarouselImg()
+    #     if len(allcarouselImg )==0:
+    #         print(u'未获取到轮播图')
+    #     else:
+    #         for carouseImg in allcarouselImg:
+    #
+
+    def test_goMainProject(self):
+        # 点击进入重点项目
+        self.homePage.goMainProjectLeft()
+        # 切换到高血压窗口tab
+        window_current = self.driver.current_window_handle
+        windows = self.driver.window_handles
+        for current_new in windows:
+            if current_new != window_current:
+                self.driver.switch_to_window(current_new)
+                self.assertIn('nccd', self.homePage.get_currentUrl(), u'跳转链接错误')
+        time.sleep(3)
+        # 切换回到首页tab
+        self.driver.switch_to_window(window_current)
+        self.homePage.goMainProjectRight()
+        self.assertTrue(self.homePage.assert_show_loginWindow(), u'登录窗口未弹出')
+
+
+
 
 
 
